c9db2f9b3c3fba59209e29a913bdcad9d12e06ea
Harsh <mekalaharshvardhanreddy@gmail.com>
2026-08-12 22:45:51 +0530

    Add real dataset footage and videos to static site demo

---
+++ site/assets/media.js
@@ -0,0 +1,21 @@
+window.DEMO_MEDIA = {
+  "5ep": {
+    label: "5ep · Take the red block and put it in the white box",
+    episodes: {
+      "0": { front: "assets/media/front/5ep_ep0.mp4", wrist: "assets/media/wrist/5ep_ep0.mp4", poster: "assets/img/5ep_ep0.jpg" },
+      "1": { front: "assets/media/front/5ep_ep1.mp4", wrist: "assets/media/wrist/5ep_ep1.mp4", poster: "assets/img/5ep_ep1.jpg" },
+      "2": { front: "assets/media/front/5ep_ep2.mp4", wrist: "assets/media/wrist/5ep_ep2.mp4", poster: "assets/img/5ep_ep2.jpg" },
+      "3": { front: "assets/media/front/5ep_ep3.mp4", wrist: "assets/media/wrist/5ep_ep3.mp4", poster: "assets/img/5ep_ep3.jpg" },
+      "4": { front: "assets/media/front/5ep_ep4.mp4", wrist: "assets/media/wrist/5ep_ep4.mp4", poster: "assets/img/5ep_ep4.jpg" }
+    }
+  },
+  "assembled_lerobot_v21": {
+    label: "assembled_lerobot_v21 · Hanoi tower.",
+    episodes: {
+      "4": { front: "assets/media/front/assembled_ep4.mp4", wrist: "assets/media/wrist/assembled_ep4.mp4", poster: "assets/img/assembled_ep4.jpg" },
+      "5": { front: "assets/media/front/assembled_ep5.mp4", poster: "assets/img/assembled_ep5.jpg" },
+      "6": { front: "assets/media/front/assembled_ep6.mp4", poster: "assets/img/assembled_ep6.jpg" },
+      "7": { front: "assets/media/front/assembled_ep7.mp4", poster: "assets/img/assembled_ep7.jpg" }
+    }
+  }
+};
